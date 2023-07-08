b205c243d8f29619c135897e51522f3dc98ef3d6
Maigwa164 <davidmaigwa164@gmail.com>
2023-07-08 20:02:45 +0300

    Declearing variables

--- HelloJava/src/Main.java
+++ HelloJava/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        int myAge = 22;
+        myAge =23;
+        int herAge = myAge;
+        System.out.println(myAge);
 
 
     }
